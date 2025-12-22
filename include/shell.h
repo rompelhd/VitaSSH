ecf37533ef3f63222d94d7d4549f04b7eee8665d
Hectorete <75935831+rompelhd@users.noreply.github.com>
2025-12-22 04:10:50 +0100

    Reorder shell function declarations

--- include/shell.h
+++ include/shell.h
@@ -5,11 +5,12 @@
 #include <vita2d.h>
 
 void shell_start_interactive();
-void shell_process_input();
-void shell_render(vita2d_pgf *font);
-void shell_handle_controls(SceCtrlData *pad, unsigned int new_buttons);
-int shell_is_active();
 void shell_stop_interactive();
 void shell_set_prompt(const char *username, const char *hostname);
+int shell_is_active();
+void shell_process_input();
+void shell_handle_controls(SceCtrlData *pad, unsigned int new_buttons);
+void shell_render(vita2d_pgf *font);
+void shell_send_special_key(const char *key_sequence);
 
 #endif
